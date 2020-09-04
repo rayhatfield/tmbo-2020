7494a2a0cddf4bbfe68617660f1a94d7bfd89b37
Ray Hatfield <ray.hatfield@gmail.com>
2020-09-04 13:52:35 -0500

    remove unused cloud functions

--- functions/index.js
+++ functions/index.js
@@ -5,21 +5,21 @@ admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
 // export const addMessage = functions.https.onRequest(async (req, res) => {
 //     const original = req.query.text;
 // })
 
-exports.generateThumbnail = functions.storage.object().onFinalize(async (object) => {
-    console.log(`  Event: ${context.eventId}`);
-    console.log(`  Event Type: ${context.eventType}`);
-    console.log(`  Bucket: ${file.bucket}`);
-    console.log(`  File: ${file.name}`);
-    console.log(`  Metageneration: ${file.metageneration}`);
-    console.log(`  Created: ${file.timeCreated}`);
-    console.log(`  Updated: ${file.updated}`);
-});
+// exports.generateThumbnail = functions.storage.object().onFinalize(async (object) => {
+//     console.log(`  Event: ${context.eventId}`);
+//     console.log(`  Event Type: ${context.eventType}`);
+//     console.log(`  Bucket: ${file.bucket}`);
+//     console.log(`  File: ${file.name}`);
+//     console.log(`  Metageneration: ${file.metageneration}`);
+//     console.log(`  Created: ${file.timeCreated}`);
+//     console.log(`  Updated: ${file.updated}`);
+// });
