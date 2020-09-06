6ed4d58311ff8ac5a994ce3bb4e81928ff3cc795
Ray Hatfield <ray.hatfield@gmail.com>
2020-09-06 03:06:17 -0500

    add tmbo client to window in api root; add welcome message

--- src/api/impl/firebase/index.js
+++ src/api/impl/firebase/index.js
@@ -4,7 +4,5 @@ import Client from './client';
 
 export default (config) => {
     const app = firebase.initializeApp(config);
-    const client = new Client(app);
-    window.tmbo = client;
     return new Client(app);
 }
--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,20 @@ import firebaseConfig from '../config/firebase';
 import firebaseClient from './impl/firebase';
 import * as EVENTS from './events';
 
+const welcome = () => {
+    if (!welcome.called) {
+        console.log('%c[+] tmbo client is exposed here for testing/debugging purposes. don’t do anything dumb.', 'color:#669;font-weight:bold');
+        welcome.called = true;
+    }
+}
+
 const client = firebaseClient(firebaseConfig);
+Object.defineProperty(window, 'tmbo', {
+    get: () => {
+        welcome();
+        return client;
+    }
+})
 
 export const ClientContext = createContext(client);
 export const useClient = () => useContext(ClientContext);
